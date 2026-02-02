d9148bace270545d5013dbb03b73c3eb557d8db7
ishan <g00562215@gmail.com>
2026-02-02 10:17:29 +0530

    Make some changes in graph

--- data_analysis.py
+++ data_analysis.py
@@ -16,7 +16,7 @@ print("Average Marks:", avg)
 print("Highest Marks:", highest)
 print("Lowest Marks:", lowest)
 
-plt.bar(data["Student_Name"],data["Total_marks"], color=["red","yellow", "blue"])
+plt.bar(data["Student_Name"],data["Total_marks"], color='skyblue', edgecolor='black', linewidth=1.5)
 plt.xlabel("Students")
 plt.xticks(rotation=45)
 plt.ylabel("Marks")
