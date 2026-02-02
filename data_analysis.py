ace401df2281da35cde98ce551614e9242901372
ishan <g00562215@gmail.com>
2026-02-02 10:11:15 +0530

    Add colour change in graph

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = pd.read_csv("Student.csv")
 
@@ -15,7 +16,7 @@ print("Average Marks:", avg)
 print("Highest Marks:", highest)
 print("Lowest Marks:", lowest)
 
-plt.bar(data["Student_Name"],data["Total_marks"], )
+plt.bar(data["Student_Name"],data["Total_marks"], color=["red","yellow", "blue"])
 plt.xlabel("Students")
 plt.xticks(rotation=45)
 plt.ylabel("Marks")
